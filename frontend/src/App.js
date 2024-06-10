321f4c53acb2a2a11a599b814be60fb1b8551e0a
FaiqAhmad <dev@ubuntu-22.0.4.myguest.virtualbox.org>
2024-06-10 10:25:51 -0600

    added the buttons and proper height formatting for the columns

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,25 @@
 
 import Stack from '@mui/material/Stack'
 import Container from '@mui/material/Container'
-// import { DataGrid } from "@mui/x-data-grid";
-
+import ButtonGroup from '@mui/material/ButtonGroup'
+import Button from '@mui/material/Button'
 
 
 function App() {
   return (
-    <Stack spacing={2} direction="row">
+    <Stack spacing={2} direction="row " maxHeight={false}>
       <Container>
-        <p> item one</p>
+        <div style={{display: 'flex', alignItems: 'center', height: '100vh'}}>
+          <ButtonGroup orientation="vertical" >
+            <Button style={{fontSize: '40px'}}>Start</Button>
+            <Button style={{fontSize: '40px'}}>Release</Button>
+            <Button style={{fontSize: '40px'}}>Stop</Button>
+            <Button style={{fontSize: '40px'}}>Pause</Button>
+            <Button style={{fontSize: '40px'}}>Kill Switch</Button>
+            <Button style={{fontSize: '40px'}}>Reset</Button>
+          </ButtonGroup>
+        </div>
+        
       </Container>
       <Container>
       <p> item two</p>
